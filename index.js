7e86cf72e0dd8135a9f93d82c55bdb0dad5a25a0
Harrison Bishop <harrisonbishsp@gmail.com>
2023-02-13 20:42:18 +0000

    changed button type to submit and changed js to use a function outside of event listener

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 let searchBar = document.getElementById("search-bar");
 const searchButton = document.getElementById("search");
 
-function newTab(){
-  window.open
+function searchQuery(){
+  searchValue = searchBar.value;
+  searchBar.value = `https://www.youtube.com/results?search_query=${searchValue}`
 }
 
 searchButton.addEventListener("click", (e) => {
   e.preventDefault();
-  searchValue = searchBar.value;
-  searchBar.value = `https://www.youtube.com/results?search_query=${searchValue}`
+  searchQuery()
   });
