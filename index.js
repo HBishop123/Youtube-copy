5da154fbeab7769ae7ef1bd373e6e4d7c3cbf365
Harrison Bishop <harrisonbishsp@gmail.com>
2023-02-13 20:54:22 +0000

    continued styling of scrollbars

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 let searchBar = document.getElementById("search-bar");
 const searchButton = document.getElementById("search");
 
-function searchQuery(){
+function searchQuery() {
   searchValue = searchBar.value;
-  searchBar.value = `https://www.youtube.com/results?search_query=${searchValue}`
+  searchBar.value = `https://www.youtube.com/results?search_query=${searchValue}`;
 }
 
 searchButton.addEventListener("click", (e) => {
   e.preventDefault();
-  searchQuery()
-  });
+  searchQuery();
+});
+
+const navBar = document.querySelector(".nav-bar-large");
+navBar.addEventListener("mouseenter", () => {
+  navBar.style.overflow = "scroll";
+});
+navBar.addEventListener("mouseleave", () => {
+  navBar.style.overflow = "hidden";
+});
+
+const categoriesSection = document.querySelector(".categories");
+categoriesSection.addEventListener("mouseenter", () => {
+  categoriesSection.style.overflow = "scroll";
+});
+
+categoriesSection.addEventListener("mouseleave", () => {
+  categoriesSection.style.overflow = "hidden";
+});
