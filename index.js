41501f9fd0aa3b71951a252e18d83ae00b0674a4
Harrison Bishop <harrisonbishsp@gmail.com>
2023-03-02 11:07:15 +0000

    converted all js into objects

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Controls the Search function of the search-bar
 const searchBarFunctionality = {
   searchBar: document.getElementById("search-bar"),
   searchButton: document.getElementById("search"),
@@ -9,7 +10,7 @@ const searchBarFunctionality = {
     } else
       this.searchBar.value = `https://www.youtube.com/results?search_query=${searchValue}`;
   },
-  
+
   addListenerToSearchBar: function () {
     this.searchButton.addEventListener("click", (e) => {
       e.preventDefault();
@@ -20,72 +21,98 @@ const searchBarFunctionality = {
 searchBarFunctionality.addListenerToSearchBar();
 
 // shows the scroll bar when section is hovered
-const navBar = document.querySelector(".nav-bar-large");
-navBar.addEventListener("mouseenter", () => {
-  navBar.style.overflowY = "scroll";
-});
-navBar.addEventListener("mouseleave", () => {
-  navBar.style.overflowY = "hidden";
-});
+const scrollBarHovering = {
+  navBar: document.querySelector(".nav-bar-large"),
 
-// chevrons
+  scrollShowing: function () {
+    this.navBar.addEventListener("mouseenter", () => {
+      this.navBar.style.overflowY = "scroll";
+    });
+    this.navBar.addEventListener("mouseleave", () => {
+      this.navBar.style.overflowY = "hidden";
+    });
+  },
+};
+scrollBarHovering.scrollShowing();
 
-const allIcons = document.querySelector(".categories");
-const suggestionsChevronLeft = document.querySelector(".scroll-left-chevron");
-const suggestionsChevronRight = document.querySelector(".scroll-right-chevron");
+// chevrons functionality
+const chevronFunctionality = {
+  allIcons: document.querySelector(".categories"),
+  suggestionsChevronLeft: document.querySelector(".scroll-left-chevron"),
+  suggestionsChevronRight: document.querySelector(".scroll-right-chevron"),
 
-suggestionsChevronLeft.addEventListener("click", () => {
-  allIcons.scrollBy({
-    left: -200,
-    behavior: "smooth",
-  });
-});
+  chevronMovement: function () {
+    this.suggestionsChevronLeft.addEventListener("click", () => {
+      this.allIcons.scrollBy({
+        left: -200,
+        behavior: "smooth",
+      });
+    });
 
-suggestionsChevronRight.addEventListener("click", () => {
-  allIcons.scrollBy({
-    left: 200,
-    behavior: "smooth",
-  });
-});
+    this.suggestionsChevronRight.addEventListener("click", () => {
+      this.allIcons.scrollBy({
+        left: 200,
+        behavior: "smooth",
+      });
+    });
+  },
+};
+chevronFunctionality.chevronMovement();
 
 // chevron to show more subs
+const showMoreChevron = {
+  showMoreContent: document.querySelector(".hidden-subscriptions"),
+  showMoreButton: document.querySelector(".listing-chevron"),
+  hideMoreContent: document.querySelector(".listing-chevron-hide"),
 
-const showMoreContent = document.querySelector(".hidden-subscriptions");
-const showMoreButton = document.querySelector(".listing-chevron");
-const hideMoreContent = document.querySelector(".listing-chevron-hide");
+  showMoreChevronFunctionality: function () {
+    this.showMoreButton.addEventListener("click", () => {
+      this.showMoreButton.style.display = "none";
+      this.showMoreContent.style.display = "flex";
+    });
 
-showMoreButton.addEventListener("click", () => {
-  showMoreButton.style.display = "none";
-  showMoreContent.style.display = "flex";
-});
-
-hideMoreContent.addEventListener("click", () => {
-  showMoreButton.style.display = "flex";
-  showMoreContent.style.display = "none";
-});
+    this.hideMoreContent.addEventListener("click", () => {
+      this.showMoreButton.style.display = "flex";
+      this.showMoreContent.style.display = "none";
+    });
+  },
+};
+showMoreChevron.showMoreChevronFunctionality();
 
 // menu button to show small or large sidebar
+const sideBarFunctionality = {
+  menuButton: document.querySelector(".menu-logo"),
+  largeNav: document.querySelector(".nav-bar-large"),
+  smallNav: document.querySelector(".nav-bar-small"),
+  videoArea: document.querySelector(".video-area"),
 
-const menuButton = document.querySelector(".menu-logo");
-const largeNav = document.querySelector(".nav-bar-large");
-const smallNav = document.querySelector(".nav-bar-small");
-const videoArea = document.querySelector(".video-area");
+  smallNavDesign: function () {
+    this.largeNav.style.display = "none";
+    this.smallNav.style.display = "flex";
+    this.videoArea.style.paddingLeft = "90px";
+    this.videoArea.style.gap = "70px";
+    this.menuButton.id = "menu-logo-small";
+  },
 
-menuButton.addEventListener("click", () => {
-  if (
-    menuButton.className === "menu-logo" &&
-    menuButton.id !== "menu-logo-small"
-  ) {
-    largeNav.style.display = "none";
-    smallNav.style.display = "flex";
-    videoArea.style.paddingLeft = "90px";
-    videoArea.style.gap = "70px";
-    menuButton.id = "menu-logo-small";
-  } else if (menuButton.id === "menu-logo-small") {
-    smallNav.style.display = "none";
-    largeNav.style.display = "flex";
-    videoArea.style.paddingLeft = "250px";
-    videoArea.style.gap = "70px";
-    menuButton.id = "";
+  largeNavDesign: function () {
+    this.smallNav.style.display = "none";
+    this.largeNav.style.display = "flex";
+    this.videoArea.style.paddingLeft = "250px";
+    this.videoArea.style.gap = "70px";
+    this.menuButton.id = "";
+  },
+
+  sideBarChanging: function () {
+    this.menuButton.addEventListener("click", () => {
+      if (
+        this.menuButton.className === "menu-logo" &&
+        this.menuButton.id !== "menu-logo-small"
+      ) {
+        this.smallNavDesign();
+      } else if (this.menuButton.id === "menu-logo-small") {
+        this.largeNavDesign();
+      }
+    });
   }
-});
+};
+sideBarFunctionality.sideBarChanging()
